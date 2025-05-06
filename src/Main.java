58e4b7d6a9e0707a0413780a1b122815844f4e60
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-05-06 17:17:59 +0300

    Save/Load

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+
+        student1.setName("Mariya");
+        student1.setGender(Gender.FEMALE);
+        student1.setSurname("Ivanova");
+        student1.setGroup(102);
+        student1.setAge(22);
+
+        var file = new File("data.txt");
+
+        save(student1, file);
+
+        var student2 = load(file, Student.class);
+        System.out.println(student2);
+    }
+
+    public static <T> void save(T object, File file){
+        var mapper = new ObjectMapper();
+        try {
+            //String s = mapper.writeValueAsString(object);
+            mapper.writeValue(file, object);
+            //System.out.println(s);
+        } catch (JsonProcessingException e) {
+            System.out.println("JSON Error");
+        } catch (IOException e) {
+            System.out.println("Save Error");
+        }
+    }
+
+    public static <T> T load(File file, Class<T> type) {
+        var mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
+        try {
+            return mapper.readValue(file, type);
+        } catch (IOException e) {
+            System.out.println("Error reading");
+            return null;
+        }
+    }
 }
+
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,58 @@
+enum Gender {
+    MALE, FEMALE
+}
 
+public class Student {
+    private String name;
+    private String surname;
+    private int age;
+    private int group;
+    private Gender gender;
 
-public class Students {
-    
+    @Override
+    public String toString() {
+        return "Name: " + name + " Surname: " + surname +
+                " Age: " + age + " Group: " + group +
+                " Gender: " + gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
